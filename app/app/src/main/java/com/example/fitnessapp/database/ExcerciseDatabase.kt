b3e4bd11d566d802d67d560b6f933a9a9c483c53
ruben godde <ruben.godde@student.uhasselt.be>
2022-05-09 14:32:04 +0200

    message

--- app/app/src/main/java/com/example/fitnessapp/database/ExcerciseDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fitnessapp.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = arrayOf(Excercise::class), version = 1)
-abstract class ExcerciseDatabase : RoomDatabase() {
-    abstract fun excerciseDao() : ExcerciseDao
-}
