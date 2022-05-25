5fda3096c8edd4ed04c7bd3db2e3cbec38d9e4e2
SikorskiJonas <jonas.sikorski@student.uhasselt.be>
2022-05-25 13:22:17 +0200

    restored to working verson

--- app/src/main/java/com/example/fitnessapp/progress/PictureAdapter.kt
+++ app/src/main/java/com/example/fitnessapp/progress/PictureAdapter.kt
@@ -8,58 +8,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnessapp.R
-import com.example.fitnessapp.Workouts.newWorkout.NewWorkoutAdapter
 import java.util.*
 
 
-class PictureAdapter(private val imageNames: MutableList<Date>, private val imagesPhoto: MutableList<Bitmap>, private var onItemClickListener: OnItemClickListener):
-    RecyclerView.Adapter<PictureAdapter.ViewHolder>() {
+class PictureAdapter (private val imageNames: MutableList<Date>, private val imagesPhoto: MutableList<Bitmap>) : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.exercise, parent, false)
-        return ViewHolder(view, onItemClickListener)
-    }
-
-    override fun getItemCount(): Int = items.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val exercise = items[position]
-        holder.bind(exercise)
-    }
-
-    class ViewHolder(itemView: View, onItemClickListener: OnItemClickListener): RecyclerView.ViewHolder(itemView){
-
-        init{
-            itemView.setOnClickListener{
-                //val snack: String = "item position clicked: $adapterPosition"
-                //Snackbar.make(itemView, snack, Snackbar.LENGTH_SHORT).show()
-                onItemClickListener.OnClick(adapterPosition)
-            }
-        }
-
-        fun bind(exercise: Exercise){
-            itemView.apply{findViewById<TextView>(R.id.txtExerciseName).text = exercise.name}
-            itemView.apply{findViewById<TextView>(R.id.txtMuscleGroup).text = exercise.muscleGroup}
-        }
-    }
-
-    interface OnItemClickListener{
-        fun OnClick(position: Int)
-    }
-
-
-}
-
-/*class PictureAdapter (private val imageNames: MutableList<Date>, private val imagesPhoto: MutableList<Bitmap>, private var onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
-
-    inner class PictureViewHolder(currentItemView: View, onItemClickListener: NewWorkoutAdapter.OnItemClickListener) : RecyclerView.ViewHolder(currentItemView)
+    inner class PictureViewHolder(currentItemView: View) : RecyclerView.ViewHolder(currentItemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.picture, parent, false)
 
-        return PictureViewHolder(view, onItemClickListener)
+        return PictureViewHolder(view)
 
     }
 
@@ -75,9 +35,4 @@ class PictureAdapter(private val imageNames: MutableList<Date>, private val imag
     override fun getItemCount(): Int {
         return imageNames.size
     }
-
-    interface OnItemClickListener{
-        fun OnClick(position: Int)
-    }
-}*/
-
+}
