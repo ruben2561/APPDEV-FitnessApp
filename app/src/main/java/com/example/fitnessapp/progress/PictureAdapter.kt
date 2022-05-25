ead38a059bcf5bae924083c20f49490cb3d09668
SikorskiJonas <jonas.sikorski@student.uhasselt.be>
2022-05-25 16:48:22 +0200

    temporary commit

--- app/src/main/java/com/example/fitnessapp/progress/PictureAdapter.kt
+++ app/src/main/java/com/example/fitnessapp/progress/PictureAdapter.kt
@@ -56,31 +56,6 @@ class PictureAdapter(private var items: List<Picture>, private var onItemClickLi
     }
 }
 
-/*class PictureAdapter (private val items: MutableList<Picture>) : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
-
-    inner class PictureViewHolder(currentItemView: View) : RecyclerView.ViewHolder(currentItemView)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.picture, parent, false)
-
-        return PictureViewHolder(view)
-
-    }
-
-    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
-        val currentItem = items[position]
-        holder.itemView.apply {
-            findViewById<TextView>(R.id.txtDate).text = currentItem.name
-            findViewById<ImageView>(R.id.galleryPicture).setImageBitmap(StringToBitMap(currentItem.imageData))
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return items.size
-    }
-}*/
-
 
 fun StringToBitMap(encodedString: String?): Bitmap? {
     return try {
