ffc9bbde6f23f4abeada23f9a5965b1c4f5bc79a
SikorskiJonas <jonas.sikorski@student.uhasselt.be>
2022-05-26 21:56:04 +0200

    google search for exercise when clicking it

--- app/src/main/java/com/example/fitnessapp/exercises/ExerciseDetailsFragment.kt
+++ app/src/main/java/com/example/fitnessapp/exercises/ExerciseDetailsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.fitnessapp.exercises
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import com.example.fitnessapp.MainActivity
 import com.example.fitnessapp.databinding.FragmentExerciseDetailsBinding
 
 
+//API key: AIzaSyAkWKRP2go7kpSiMS5sY2N--3UvHgi_cTU
 class ExercisesDetailsFragment(val exercise: Exercise) : Fragment() {
 
     private lateinit var binding: FragmentExerciseDetailsBinding
@@ -22,6 +25,11 @@ class ExercisesDetailsFragment(val exercise: Exercise) : Fragment() {
         binding = FragmentExerciseDetailsBinding.inflate(layoutInflater)
         parentActivity = activity as MainActivity
         binding.txtChosenExercise.text = exercise.name
+        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+        intent.putExtra(SearchManager.QUERY, exercise.name + " exercise")
+        startActivity(intent)
+
+
         return binding.root
     }
 }
